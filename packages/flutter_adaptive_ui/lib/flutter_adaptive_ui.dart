94a20685c9f2759668f3b84f9bcbaf98904436d8
Mohammad Taheri <taherimohammad08@gmail.com>
2022-05-24 01:07:52 +0430

    Add debugLabel to DeviceType

--- packages/flutter_adaptive_ui/lib/flutter_adaptive_ui.dart
+++ packages/flutter_adaptive_ui/lib/flutter_adaptive_ui.dart
@@ -3,5 +3,5 @@ library flutter_adaptive_ui;
 import 'package:flutter/material.dart';
 
 part './src/device_type.dart';
-part './src/window_size.dart';
+part 'src/window_type.dart';
 part './src/breakpoint.dart';
--- packages/flutter_adaptive_ui/lib/src/device_type.dart
+++ packages/flutter_adaptive_ui/lib/src/device_type.dart
@@ -1,14 +1,15 @@
 part of flutter_adaptive_ui;
 
 enum DeviceType {
-  smallHandset(0, 359),
-  mediumHandset(360, 399),
-  largeHandset(400, 599),
-  smallTablet(600, 719),
-  largeTablet(720, 1023),
-  desktop(1024, double.infinity);
+  smallHandset(0, 359, 'Small Handset'),
+  mediumHandset(360, 399, 'Medium Handset'),
+  largeHandset(400, 599, 'Large Handset'),
+  smallTablet(600, 719, 'Small Tablet'),
+  largeTablet(720, 1023, 'Large Tablet'),
+  desktop(1024, double.infinity, 'Desktop');
 
-  const DeviceType(this.minWidth, this.maxWidth);
+  const DeviceType(this.minWidth, this.maxWidth, [this.debugLabel]);
   final double minWidth;
   final double maxWidth;
+  final String? debugLabel;
 }
