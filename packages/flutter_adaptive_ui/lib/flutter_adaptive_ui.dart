661425dadbaedfb49a9fd9d8eaa0e9b3f1a8dd45
Mohammad Taheri <taherimohammad08@gmail.com>
2022-05-24 11:43:06 +0430

    Create device.dart

--- packages/flutter_adaptive_ui/lib/flutter_adaptive_ui.dart
+++ packages/flutter_adaptive_ui/lib/flutter_adaptive_ui.dart
@@ -2,6 +2,4 @@ library flutter_adaptive_ui;
 
 import 'package:flutter/material.dart';
 
-part './src/device_type.dart';
-part 'src/window_type.dart';
-part './src/breakpoint.dart';
+part './src/device.dart';
---
+++ packages/flutter_adaptive_ui/lib/src/device.dart
@@ -0,0 +1,34 @@
+part of flutter_adaptive_ui;
+
+class Device {}
+
+enum WindowSize {
+  xsmall(0, 599, 'XSmall'),
+  small(600, 1023, 'Small'),
+  medium(1024, 1439, 'Medium'),
+  large(1440, 1919, 'Large'),
+  xlarge(1920, double.infinity, 'XLarge');
+
+  const WindowSize(this.minWidth, this.maxWidth, [this.debugLabel]);
+  final double minWidth;
+  final double maxWidth;
+  final String? debugLabel;
+}
+
+enum WindowType {
+  smallHandset(0, 359, 'Small Handset'),
+  mediumHandset(360, 399, 'Medium Handset'),
+  largeHandset(400, 599, 'Large Handset'),
+  smallTablet(600, 719, 'Small Tablet'),
+  largeTablet(720, 1023, 'Large Tablet'),
+  desktop(1024, double.infinity, 'Desktop');
+
+  const WindowType(this.minWidth, this.maxWidth, [this.debugLabel]);
+  final double minWidth;
+  final double maxWidth;
+  final String? debugLabel;
+}
+
+enum DeviceType { handset, tablet, desktop }
+
+enum PlatformType { android, ios, windows, macos, linux, web }
