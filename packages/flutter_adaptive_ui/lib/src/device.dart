01a7df45d89e32b84ce2d7152434614760489a81
Mohammad Taheri <taherimohammad08@gmail.com>
2022-05-24 14:16:01 +0430

    Update Device

--- packages/flutter_adaptive_ui/lib/src/device.dart
+++ packages/flutter_adaptive_ui/lib/src/device.dart
@@ -1,3 +1,27 @@
 part of flutter_adaptive_ui;
 
-class Device {}
+class Device {
+  Device(BuildContext context) {
+    mediaQuery = MediaQuery.of(context);
+    breakpoint = Breakpoint.fromMediaQuery(context);
+    targetPlatform = defaultTargetPlatform;
+    isWeb = kIsWeb;
+  }
+
+  late final MediaQueryData mediaQuery;
+  late final Breakpoint breakpoint;
+  late final TargetPlatform targetPlatform;
+  late final bool isWeb;
+
+  WindowSize get windowSize => breakpoint.windowSize;
+  WindowType get windowType => breakpoint.windowType;
+  int get columns => breakpoint.columns;
+  double get gutters => breakpoint.gutters;
+  double get margins => breakpoint.margins;
+
+  double get width => mediaQuery.size.width;
+  double get height => mediaQuery.size.height;
+  double get pixelRatio => mediaQuery.devicePixelRatio;
+  double get physicalWidth => width * pixelRatio;
+  double get physicalHeight => height * pixelRatio;
+}
