3929e74fe81eb58642b74b7aa7c371d5ab328c50
Mohammad Taheri <taherimohammad08@gmail.com>
2022-05-26 02:37:24 +0430

    Doc: Remove documents from [Screen]

--- packages/flutter_adaptive_ui/lib/src/device.dart
+++ packages/flutter_adaptive_ui/lib/src/device.dart
@@ -21,18 +21,10 @@ class Screen {
     );
   }
 
-  /// xsmall , small , medium , large , xlarge
   final ScreenSize screenSize;
 
-  /// small handset , medium handset , large hanset
-  /// small tablet , large tablet
-  /// small desktop , medium desktop , large desktop
   final ScreenType screenType;
 
-  /// If [targetPlatform] is android or fuchsia, it will be [DesignLanguage.material]
-  /// If [targetPlatform] is ios or macos, it will be [DesignLanguage.cupertino]
-  /// If [targetPlatform] is windows, it will be [DesignLanguage.fluent]
-  /// For other [targetPlatform]s (linux , web), it will be [DesignLanguage.material]
   late final DesignLanguage designLanguage;
 
   late final PlatformType platform;
