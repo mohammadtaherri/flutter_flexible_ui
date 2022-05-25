cce7a6b745726e0adf1d28b0d22e04de8fd639cd
Mohammad Taheri <taherimohammad08@gmail.com>
2022-05-25 15:07:25 +0430

    Remove minWidth and maxWidth from ScreenSize and ScreenType

--- packages/flutter_adaptive_ui/lib/src/types.dart
+++ packages/flutter_adaptive_ui/lib/src/types.dart
@@ -1,31 +1,28 @@
 part of flutter_adaptive_ui;
 
 enum ScreenSize {
-  xsmall(0, 599, 'XSmall'),
-  small(600, 1023, 'Small'),
-  medium(1024, 1439, 'Medium'),
-  large(1440, 1919, 'Large'),
-  xlarge(1920, double.infinity, 'XLarge');
+  xsmall('XSmall'),
+  small('Small'),
+  medium('Medium'),
+  large('Large'),
+  xlarge('XLarge');
 
-  const ScreenSize(this.minWidth, this.maxWidth, [this.debugLabel]);
-  final double minWidth;
-  final double maxWidth;
+  const ScreenSize([this.debugLabel]);
   final String? debugLabel;
 }
 
 enum ScreenType {
-  smallHandset(0, 359, 'Small Handset'),
-  mediumHandset(360, 399, 'Medium Handset'),
-  largeHandset(400, 599, 'Large Handset'),
-  smallTablet(600, 719, 'Small Tablet'),
-  largeTablet(720, 1023, 'Large Tablet'),
-  smallDesktop(1024, 1439, 'Small Desktop'),
-  mediumDesktop(1440, 1919, 'Medium Desktop'),
-  largeDesktop(1920, double.infinity, 'Large Desktop');
+  smallHandset('Small Handset'),
+  mediumHandset('Medium Handset'),
+  largeHandset('Large Handset'),
+  smallTablet('Small Tablet'),
+  largeTablet('Large Tablet'),
+  smallDesktop('Small Desktop'),
+  mediumDesktop('Medium Desktop'),
+  largeDesktop('Large Desktop');
 
-  const ScreenType(this.minWidth, this.maxWidth, [this.debugLabel]);
-  final double minWidth;
-  final double maxWidth;
+  const ScreenType([this.debugLabel]);
+
   final String? debugLabel;
 }
 
